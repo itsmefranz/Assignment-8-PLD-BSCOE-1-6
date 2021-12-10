eb322dbf8961296b8b48523668427cf505172ddd
Daniella Francine Velasquez <daniellafrancine.velasquez@gmail.com>
2021-12-11 02:13:49 +0800

    redid the whole code just because it wont work

--- program2.py
+++ program2.py
@@ -1,23 +1,45 @@
+# Program 2: Guess the number
+# Generate 1 random number (0-100)
+# Ask the user to guess the number
+# Display “Greater than” if the inputed number is greater than the random number
+# Display “Less than” if the inputed number is less than the random number
+# Repeat asking the user until the random number has been guessed correctly.
 import random
 
-def wholeplay():
-    while True: 
-        try: 
-            x= random.sample(range(0,100))
-            guess= int(input("Guess a number and enter: "))
+def play():
+    while True:
+        try:
+            guess= int(input("Enter your guess: "))
+            if guess not in range(0,100):
+                print("Invalid input. Number is out of range.")
+                continue
         except ValueError:
-            print("Invalid input. Please do enter a number to try again.")
+            print("Invalid input. Please do enter a number.")
             continue
         else:
             break
-    return x, guess
-    
-# Condition testing
-    if x == guess:
-        print("Congratulations you got it!")
-    # Once guessed, loop will break
+    return guess
+
+def guess():
+    correctNum= random.sample(range(0,100),1)
+    return correctNum
+
+def onceMore():
+     while True:
+        yn = input("Do you wish to try again?: ")
+        yn = yn.lower()
+        if yn == 'y':
+            print("\n")
+            play()
+            guess()
+        elif yn == 'n':
+            print("Thanks for playing!")
+            break
+        else:
+            print("\n")
+            print("Please enter y if you want to play again, or enter n to exit the game.")
         break
-    elif x > guess:
-        print("Your number is greater than the correct answer.")
-    elif x < guess:
-        print("Your number is less than the correct answer.")
+
+userguess= play()
+correctguess= guess()
+onceMore()
