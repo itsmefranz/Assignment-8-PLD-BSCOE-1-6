245fb51f9b34c113113a6dfe6832cd1e94f26d2d
Daniella Francine Velasquez <daniellafrancine.velasquez@gmail.com>
2021-12-11 01:21:57 +0800

    defined checker funstion for usernums and winning nums to display if they win or lose ++ defined winning numbers

--- program1.py
+++ program1.py
@@ -9,6 +9,20 @@
 
 import random
 
+# 'checker', compares userNums and winningNums to see if they have won or lost
+def checker(userNums, winningNums):
+
+    if userNums == winningNums:
+        print ("\nCongratulations! You Win!\n")
+        print ("Your numbers: ", userNums)
+        print ("The winning lottery numbers are: ", winningNums, "\n")
+    else:
+
+        print ("\nSorry, you lose...\n")
+        print ("Your numbers: ", userNums)
+        print ("The winning lottery numbers are: ", winningNums, "\n")
+
+# program asks user to input three number along with input validation
 def askNums():
     while True:
         try:
@@ -21,3 +35,23 @@ def askNums():
                 print("Invalid input. Number is out of range.")
                 continue
             thirdnum= int(input("Enter your third number; any number ranging from 0-9: "))
+            if thirdnum not in range(0,9):
+                print("Invalid input. Number is out of range.")
+                continue
+        except ValueError:
+            print("Invalid input. Please do enter a number.")
+            continue
+        else:
+            break
+    numList= [firstnum, secondnum,thirdnum]
+    return numList
+
+# program generates winning numbers that would be random three
+def getWinningNums():
+    winnum1, winnum2, winnum3= random.sample(range(0,9),3)
+    winningList= [winnum1, winnum2, winnum3]
+    return winningList
+
+userNums= askNums()
+winningNums = getWinningNums()
+checker(userNums, winningNums)
