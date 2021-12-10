93754380604f969f3d8c02986718c0ec554fd33b
Daniella Francine Velasquez <daniellafrancine.velasquez@gmail.com>
2021-12-11 01:48:13 +0800

    finished defining wholeplay function

--- program2.py
+++ program2.py
@@ -1,13 +1,23 @@
-# Condition testing
+import random
+
 def wholeplay():
     while True: 
         try: 
-            int(input("Guess a number and enter: "))
-            if x == guess:
-                print("Congratulations you got it!")
-                # Once guessed, loop will break
-                break
-            elif x > guess:
-                print("Your number is greater than the correct answer.")
-            elif x < guess:
-                print("Your number is less than the correct answer.")
+            x= random.sample(range(0,100))
+            guess= int(input("Guess a number and enter: "))
+        except ValueError:
+            print("Invalid input. Please do enter a number to try again.")
+            continue
+        else:
+            break
+    return x, guess
+    
+# Condition testing
+    if x == guess:
+        print("Congratulations you got it!")
+    # Once guessed, loop will break
+        break
+    elif x > guess:
+        print("Your number is greater than the correct answer.")
+    elif x < guess:
+        print("Your number is less than the correct answer.")
